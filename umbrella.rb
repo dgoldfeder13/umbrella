fe541cb1a28f2e37de5eb4123a402dbe8b78f392
dgoldfeder13 <114775261+dgoldfeder13@users.noreply.github.com>
2025-01-27 22:08:27 +0000

    Finished

--- umbrella.rb
+++ umbrella.rb
@@ -5,8 +5,8 @@ require "dotenv/load"
 pp "howdy"
 pp "Where are you located?"
 
-#user_location = gets.chomp
-user_location = "Chicago"
+user_location = gets.chomp
+#user_location = "Chicago"
 pp user_location
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY") 
@@ -26,3 +26,12 @@ lat = location.fetch("lat")
 long = location.fetch("lng")
 pp lat
 pp long
+
+pirate_url = "https://api.pirateweather.net/forecast/" + ENV.fetch("PIRATE_KEY") + "/" + lat.to_s + "," + long.to_s
+pp pirate_url
+
+pirate_resp = HTTP.get(pirate_url)
+raw_response = pirate_resp.to_s
+parsed_response = JSON.parse(raw_response)
+current = parsed_response.fetch("currently")
+pp "The Temperature is " + current.fetch("temperature").to_s + " degrees Farenheit"
