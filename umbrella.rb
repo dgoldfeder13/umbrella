5bef9cda1b0f4ef747c97d4aa30252023f47f553
dgoldfeder13 <114775261+dgoldfeder13@users.noreply.github.com>
2025-01-27 21:51:06 +0000

    got tutorial to work

--- umbrella.rb
+++ umbrella.rb
@@ -3,3 +3,26 @@ require "http"
 require "json"
 require "dotenv/load"
 pp "howdy"
+pp "Where are you located?"
+
+#user_location = gets.chomp
+user_location = "Chicago"
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY") 
+
+resp = HTTP.get(maps_url)
+
+raw_response = resp.to_s
+parsed_response = JSON.parse(raw_response)
+results_hash = parsed_response.fetch("results")
+
+first_result = results_hash.at(0)
+
+geometry = first_result.fetch("geometry")
+location = geometry.fetch("location")
+
+lat = location.fetch("lat")
+long = location.fetch("lng")
+pp lat
+pp long
